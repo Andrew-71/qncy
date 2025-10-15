f70d221b4a38d81c1f2b4b8c866dbc75490e5f4c
Andrey Nikitin <andrew@a71.su>
2025-10-15 23:45:59 +0300

    Initial commit

---
+++ qncy/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator
+from qncy.models import Question, Tag, User
+
+from django.db.models import Count
+
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def common_context():
+    top_tags = Tag.objects.annotate(
+        uses=Count('question')).order_by('-uses')[:20]
+    top_users = User.objects.annotate( # FIXME: this is NOT what we want.
+        rating=Count('answer')).order_by('-rating')[:10]
+
+    return {
+        "common": {
+            "top_tags": top_tags,
+            "top_users": top_users
+        }
+    }
+
+def index(request):
+    # There's probably a more pythonistic way to do this
+    page = 1
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        pass
+    page_size = 20
+    try:
+        page_size = int(request.GET.get("pagesize", 20))
+    except ValueError:
+        pass
+    # NOTE: we *might* add different ways to sort eventually and this will
+    # be likely decided here in another query parameter.
+
+    latest_question_list = Question.objects.order_by("-created_at")
+    paginator = Paginator(latest_question_list, page_size)
+    page_obj = paginator.get_page(page)
+    context = {
+        "page_obj": page_obj,
+    }
+    return render(request, "qncy/index.html", context | common_context())
+
+def question(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "qncy/question.html", {"question": question} | common_context())
+
+def tag(request, tag_name):
+    try:
+        tag = Tag.objects.get(name=tag_name)
+    except Tag.DoesNotExist:
+        raise Http404("Tag does not exist")
+    return tag.name
+    # return render(request, "qncy/question.html", {"question": question} | common_context())
+
+def logout_view(request):
+    logout(request)
+    return render(request, "qncy/logged_out.html", common_context())
