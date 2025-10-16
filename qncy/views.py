54a81fe2fd8a80398b8733103f9d7fe618386885
Andrey Nikitin <andrew@a71.su>
2025-10-16 21:08:35 +0300

    Add voting buttons and improve question view

--- qncy/views.py
+++ qncy/views.py
@@ -4,13 +4,28 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse, Http404
 from django.core.paginator import Paginator
-from qncy.models import Question, Tag, User, QuestionForm
+from qncy.models import Question, Tag, User, QuestionForm, Answer
 
 from django.db.models import Count
 
 from django.contrib.auth import logout
 
 from vkhw import settings
+
+def paginator_page(request, objects):
+    # There's probably a more pythonistic way to do this
+    page = 1
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        pass
+    page_size = 20
+    try:
+        page_size = int(request.GET.get("pagesize", 20))
+    except ValueError:
+        pass
+    paginator = Paginator(objects, page_size)
+    return paginator.get_page(page)
 
 def common_context():
     top_tags = Tag.objects.annotate(
@@ -26,36 +41,30 @@ def common_context():
     }
 
 def index(request):
-    # There's probably a more pythonistic way to do this
-    page = 1
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        pass
-    page_size = 20
-    try:
-        page_size = int(request.GET.get("pagesize", 20))
-    except ValueError:
-        pass
-    # NOTE: we *might* add different ways to sort eventually and this will
-    # be likely decided here in another query parameter.
-
+    # TODO: sorting?
     latest_question_list = Question.objects.order_by("-created_at").annotate(
         answers=Count('answer'),
     )
-    paginator = Paginator(latest_question_list, page_size)
-    page_obj = paginator.get_page(page)
     context = {
-        "page_obj": page_obj,
+        "page_obj": paginator_page(request, latest_question_list),
     }
     return render(request, "qncy/index.html", context | common_context())
 
 def question(request, question_id):
     try:
-        question = Question.objects.get(pk=question_id)
+        question = Question.objects.annotate(
+            answers=Count('answer'),
+        ).get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, "qncy/question.html", {"question": question} | common_context())
+
+    answers_list = Answer.objects.filter(question=question).order_by("-rating")
+    context = {
+        "question": question,
+        "page_obj": paginator_page(request, answers_list),
+    }
+
+    return render(request, "qncy/question.html", context | common_context())
 
 def tag(request, tag_name):
     try:
