732d225350c726f94715858e036a3fee6848d35e
Andrey Nikitin <andrew@a71.su>
2025-10-23 20:19:41 +0300

    Set Tag name as unique

--- qncy/models.py
+++ qncy/models.py
@@ -7,7 +7,7 @@ from core.models import User
 
 # Tag: ...tag
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
