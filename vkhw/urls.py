2ba3f0316f66de1f9fe7200c4db86d8c42e24845
Andrey Nikitin <andrew@a71.su>
2025-12-25 16:05:19 +0300

    Add test results

--- vkhw/urls.py
+++ vkhw/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
@@ -14,4 +15,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += debug_toolbar_urls()
